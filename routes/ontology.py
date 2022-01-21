a3871ef7d6eb738742d3366b647ccab88d081bcb
elskater98 <elskater98@gmail.com>
2022-01-21 17:03:18 +0100

    get classes and all properties (object, data, annotation)

--- routes/ontology.py
+++ routes/ontology.py
@@ -12,7 +12,22 @@ def get_classes():
     return jsonify(data=[classe.name for classe in list(ontology.classes())])
 
 
-@ontology_router.route("/object/properties", methods=["GET"])
+@ontology_router.route("/properties/<property_type>", methods=["GET"])
 @jwt_required()
-def get_object_properties():
-    return jsonify(data=[classe.name for classe in list(ontology.classes())])
+def get_object_properties(property_type):
+    properties = []
+    if property_type == 'data':
+        properties = list(ontology.data_properties())
+
+    if property_type == 'object':
+        properties = list(ontology.object_properties())
+
+    if property_type == 'annotation':
+        properties = list(ontology.annotation_properties())
+
+    if property_type == 'all':
+        properties = list(ontology.properties())
+
+    return jsonify(
+        data=[{"name": property.name, "range": str(property.range)[1:-1],
+               "domain": str(property.domain)[1:-1]} for property in properties])
