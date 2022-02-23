cf99dba383eb594066aab370ffc00463d72f687e
elskater98 <elskater98@gmail.com>
2022-02-23 15:58:34 +0100

    - update heroku conf.

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
+import os
 from app import app
 
 if __name__ == '__main__':
     # https://flask.palletsprojects.com/en/2.0.x/config/
-    app.run()
+    app.run(host=os.getenv('SERVER_HOSTNAME', default='127.0.0.1'), port=int(os.getenv('SERVER_PORT', default='5000')))
