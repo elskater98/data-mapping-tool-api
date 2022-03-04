22e2731ffd30f9a917a44cebae15030a14cdca73
elskater98 <elskater98@gmail.com>
2022-03-04 11:34:13 +0100

    Restructure code

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
-from app import app
+from app import create_app
 
 if __name__ == '__main__':
     # https://flask.palletsprojects.com/en/2.0.x/config/
-    app.run(host=os.getenv('SERVER_HOSTNAME', default='127.0.0.1'), port=int(os.getenv('SERVER_PORT', default='5000')))
+    create_app().run(host=os.getenv('SERVER_HOSTNAME', default='127.0.0.1'), port=int(os.getenv('SERVER_PORT', default='5000')))
