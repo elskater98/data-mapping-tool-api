b9072c57711affe5e5ed32a9028e332f81f3b8e7
elskater98 <elskater98@gmail.com>
2022-04-07 16:48:59 +0200

    Adapt old methods to isolate ontology

--- routes/ontology.py
+++ routes/ontology.py
@@ -1,98 +1,89 @@
 import datetime
-import os
-from flask import send_file
+
 from bson import ObjectId
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from owlready2 import default_world, get_ontology
 
 from database import mongo
-from models.instance import InstanceModel
 from models.ontology import OntologyModel, VisibilityEnum
-from utils import get_user_by_username, parse_json, remove_file, get_file
+from utils import get_user_by_username, parse_json, remove_file, get_file, define_ontology
 
 ontology_router = Blueprint('ontology', __name__)
 
 
-# ontology = get_ontology(os.getenv("ONTOLOGY_PATH", os.path.join(os.path.abspath(os.getcwd()), "ontology.owl"))).load()
-#
-#
-# @ontology_router.route("/query", methods=["POST"])
-# @jwt_required()
-# def sparql_query():
-#     data = list(default_world.sparql(request.json['query']))
-#     return jsonify(data=data.__str__())
-#
-#
-# @ontology_router.route("/classes", methods=["GET"])
-# @jwt_required()
-# def get_classes():
-#     return jsonify(data=[{"label": str(i), "value": str(i)} for i in list(ontology.classes())])
-#
-#
-# @ontology_router.route("/relations", methods=["GET"])
-# @jwt_required()
-# def get_classes_relations():
-#     relations = [{"class": str(i), "relations": list(i._get_class_possible_relations()).__str__()[1:-1].split(',')} for
-#                  i in list(ontology.classes())]
-#     return jsonify(data=relations)
-#
-#
-# @ontology_router.route("/properties/<property_type>", methods=["GET"])
-# @jwt_required()
-# def get_object_properties(property_type):
-#     properties = []
-#
-#     if property_type == 'data':
-#         properties = list(ontology.data_properties())
-#
-#     if property_type == 'object':
-#         properties = list(ontology.object_properties())
-#
-#     if property_type == 'annotation':
-#         properties = list(ontology.annotation_properties())
-#
-#     if property_type == 'all':
-#         properties = list(ontology.properties())
-#
-#     if 'classes' in request.args:
-#         classes = request.args['classes'].split(',')
-#         properties = [elem for elem in properties if str(elem.domain)[1:-1] in classes]
-#
-#     return jsonify(
-#         data=[
-#             {"name": i.name,
-#              "value": str(i.domain)[1:-1] + ':' + i.name,
-#              "range": str(i.range)[1:-1],
-#              "domain": str(i.domain)[1:-1]} for i in properties])
-#
-#
-# @ontology_router.route("/classes/relations", methods=["POST"])
-# @jwt_required()
-# def get_relations():
-#     req = request.json
-#     relations = {}
-#     for i in ontology.object_properties():
-#         if str(i.domain[0]) in req['classes'] and str(i.range[0]) in req['classes']:
-#             rel = {"from": str(i.domain[0]), "to": str(i.range[0]), "relation": str(i)}
-#             relations.update({str(i): rel})
-#
-#     return jsonify(successful=True, relations=relations)
-#
-#
-# @ontology_router.route("/view", methods=["GET"])
-# def get_ontology_view():
-#     classes = [{'id': str(_class), 'data': {'label': str(_class)}, 'position': {'x': 0, 'y': 0}} for _class in
-#                ontology.classes()]
-#
-#     relations = [
-#         {"source": str(i.domain[0]), "target": str(i.range[0]), "id": str(i), "label": str(i), 'type': 'smooth',
-#          'style': {'stroke': 'black'}, 'arrowHeadType': 'arrowclosed', 'animated': True} for i in
-#         ontology.object_properties()]
-#
-#     return jsonify(classes=classes, relations=relations)
-#
-#
+@ontology_router.route("/<id>/classes", methods=["GET"])
+@jwt_required()
+def get_classes(id):
+    ontology = define_ontology(id)
+    return jsonify(data=[{"label": str(i), "value": str(i)} for i in list(ontology.classes())])
+
+
+@ontology_router.route("/<id>/relations", methods=["GET"])
+@jwt_required()
+def get_classes_relations(id):
+    ontology = define_ontology(id)
+    relations = [{"class": str(i), "relations": list(i._get_class_possible_relations()).__str__()[1:-1].split(',')} for
+                 i in list(ontology.classes())]
+    return jsonify(data=relations)
+
+
+@ontology_router.route("/<id>/properties/<property_type>", methods=["GET"])
+@jwt_required()
+def get_object_properties(id, property_type):
+    ontology = define_ontology(id)
+    properties = []
+
+    if property_type == 'data':
+        properties = list(ontology.data_properties())
+
+    if property_type == 'object':
+        properties = list(ontology.object_properties())
+
+    if property_type == 'annotation':
+        properties = list(ontology.annotation_properties())
+
+    if property_type == 'all':
+        properties = list(ontology.properties())
+
+    if 'classes' in request.args:
+        classes = request.args['classes'].split(',')
+        properties = [elem for elem in properties if str(elem.domain)[1:-1] in classes]
+
+    return jsonify(
+        data=[
+            {"name": i.name,
+             "value": str(i.domain)[1:-1] + ':' + i.name,
+             "range": str(i.range)[1:-1],
+             "domain": str(i.domain)[1:-1]} for i in properties])
+
+
+@ontology_router.route("/<id>/classes/relations", methods=["POST"])
+@jwt_required()
+def get_relations(id):
+    req = request.json
+    relations = {}
+    ontology = define_ontology(id)
+
+    for i in ontology.object_properties():
+        if str(i.domain[0]) in req['classes'] and str(i.range[0]) in req['classes']:
+            rel = {"from": str(i.domain[0]), "to": str(i.range[0]), "relation": str(i)}
+            relations.update({str(i): rel})
+
+    return jsonify(successful=True, relations=relations)
+
+
+@ontology_router.route("/<id>/view", methods=["GET"])
+def get_ontology_view(id):
+    ontology = define_ontology(id)
+    classes = [{'id': str(_class), 'data': {'label': str(_class)}, 'position': {'x': 0, 'y': 0}} for _class in
+               ontology.classes()]
+
+    relations = [
+        {"source": str(i.domain[0]), "target": str(i.range[0]), "id": str(i), "label": str(i), 'type': 'smooth',
+         'style': {'stroke': 'black'}, 'arrowHeadType': 'arrowclosed', 'animated': True} for i in
+        ontology.object_properties()]
+
+    return jsonify(classes=classes, relations=relations)
 
 
 @ontology_router.route("/<ontology>", methods=["POST"])
